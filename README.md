463e5514c0865092f2029754c54d4f42a03507b0
织梦行云之唯心所识 <313097973@qq.com>
2022-09-02 21:29:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# firecms-ext/mail
+
+```shell
+# 依赖安装
+composer require firecms-ext/mail
+```
+
+# 函数列表
+
+```php
+
+```
---
+++ src/ConfigProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of FirecmsExt Mail.
+ *
+ * @link     https://www.klmis.cn
+ * @document https://www.klmis.cn
+ * @contact  zhimengxingyun@klmis.cn
+ * @license  https://github.com/firecms-ext/mail/blob/master/LICENSE
+ */
+namespace FirecmsExt\Utils;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+            ],
+            'commands' => [
+            ],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
