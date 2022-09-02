f20b1459e8ffc63c844f050b5acea73a2852dfc3
织梦行云之唯心所识 <313097973@qq.com>
2022-09-02 21:42:01 +0800

    init

--- tests/Cases/FunctionTest.php
+++ tests/Cases/FunctionTest.php
@@ -17,51 +17,9 @@ namespace HyperfTest\Cases;
  */
 class FunctionTest extends AbstractTestCase
 {
-    public function testAge()
+    public function test()
     {
-        $birthday = date('Y-m-d', strtotime('-1 year -1 month  -1 day'));
-        // var_dump($birthday);
-        $this->assertEquals(1, age($birthday));
+        $this->assertIsInt(1);
     }
 
-    public function testUuid()
-    {
-        $uuid = uuid();
-        // var_dump($uuid);
-        $this->assertIsString($uuid);
-    }
-
-    public function testFilesizeFormat()
-    {
-        $str = filesizeFormat(1024 * 1022 * 10);
-        // var_dump($str);
-        $this->assertIsString($str);
-        $this->assertEquals('9.98 MB', $str);
-    }
-
-    public function testToTree()
-    {
-        $tree = toTree([
-            ['id' => 1, 'parent_id' => null],
-            ['id' => 2, 'parent_id' => 1],
-            ['id' => 3, 'parent_id' => 2],
-            ['id' => 4, 'parent_id' => 1],
-        ]);
-        // var_dump($tree);
-        $this->assertIsArray($tree);
-        $this->assertEquals([
-            [
-                'id' => 1, 'parent_id' => null,
-                'children' => [
-                    [
-                        'id' => 2, 'parent_id' => 1,
-                        'children' => [
-                            ['id' => 3, 'parent_id' => 2],
-                        ],
-                    ],
-                    ['id' => 4, 'parent_id' => 1],
-                ],
-            ],
-        ], $tree);
-    }
 }
